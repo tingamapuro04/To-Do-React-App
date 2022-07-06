be974e0f9e2bf690cbc5ba6029d463e76543cf43
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-06 12:21:06 +0300

    use of class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React, { Component } from 'react';
+import Todo from './components/todo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1>Hello</h1>
+        <Todo />
       </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Todo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Kichwa</h1>
+      </div>
+    );
+  }
+}
+
+export default Todo;
