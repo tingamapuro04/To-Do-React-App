5bcbad2f1ccaa708f7503d4b818acb4c986d459f
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-06 12:26:53 +0300

    Add state

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,6 +3,25 @@ import React, { Component } from 'react';
 
 
 class Todo extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Takeout trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Eat something',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Sleep',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
